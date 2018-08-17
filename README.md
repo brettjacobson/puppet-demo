3ccd5fe3157e15b5fee69a5667f2084eab97eedb
Brett Jacobson <brett_jacobson@dell.com>
2018-08-17 10:35:46 -0500

    updates to properly unit test the scenario

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+# Puppet Demo code for unit test scenario
+
 This is an example of a problem unit testing a certain scenario in rspec-puppet
 
 https://github.com/rodjek/rspec-puppet/issues/715
+
+Now the code is "fixed" so that it works!
--- spec/classes/test1_spec.rb
+++ spec/classes/test1_spec.rb
@@ -7,12 +7,12 @@ describe 'testdemo::test1' do
       let(:pre_condition) { "service {'test2svc':}"}
       let(:params) do
         {
-          notifications: ["Service[test2svc]",]
+          notifications: [ref('Service', 'test2svc')]
         }
       end
 
       it { is_expected.to compile }
-      it { is_expected.to contain_exec('sample-exec').with_notify("Service[test2svc]") }
+      it { is_expected.to contain_exec('sample-exec').that_notifies('Service[test2svc]') }
     end
   end
 end
